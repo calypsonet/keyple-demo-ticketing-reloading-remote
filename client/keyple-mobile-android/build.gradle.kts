9071ea47d3907cab2cf26fef2d7aa9c5ce65b309
Jean-Pierre Fortune <jean-pierre.fortune@ialto.com>
2025-07-10 10:09:54 +0200

    wip

--- client/keyple-mobile-android/build.gradle.kts
+++ client/keyple-mobile-android/build.gradle.kts
@@ -1,48 +1,15 @@
 ///////////////////////////////////////////////////////////////////////////////
 //  GRADLE CONFIGURATION
 ///////////////////////////////////////////////////////////////////////////////
+
 plugins {
-    id("com.diffplug.spotless") version "6.25.0"
+    id("com.diffplug.spotless") version "7.0.4"
+    id("org.jetbrains.dokka") version "1.9.20"
 }
 buildscript {
-    val kotlinVersion: String by project
-    repositories {
-        mavenLocal()
-        mavenCentral()
-        google()
-    }
     dependencies {
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.20")
         classpath("com.android.tools.build:gradle:7.4.2")
-        classpath("javax.xml.bind:jaxb-api:2.3.1")
-        classpath("com.sun.xml.bind:jaxb-impl:2.3.9")
     }
 }
 
-///////////////////////////////////////////////////////////////////////////////
-//  APP CONFIGURATION
-///////////////////////////////////////////////////////////////////////////////
-allprojects {
-    group = "org.calypsonet.keyple"
-    repositories {
-        mavenLocal()
-        mavenCentral()
-        maven(url = "https://nexus.coppernic.fr/repository/libs-release")
-        maven(url = "https://central.sonatype.com/repository/maven-snapshots/")
-        maven(url = "https://oss.sonatype.org/content/repositories/snapshots/")
-        google()
-    }
-}
-
-///////////////////////////////////////////////////////////////////////////////
-//  TASKS CONFIGURATION
-///////////////////////////////////////////////////////////////////////////////
-tasks {
-    spotless {
-        kotlin {
-            target("**/*.kt")
-            ktfmt()
-            licenseHeaderFile("${project.rootDir}/LICENSE_HEADER_BSD_3-CLAUSE")
-        }
-    }
-}
